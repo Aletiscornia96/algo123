ba1863bc8ee5d377d31f01a36d5b7cd1ea72791f
Ale <aletiscornia96@gmail.com>
2024-09-04 11:46:40 -0300

    actualizacion de schema

--- models/News.js
+++ models/News.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const NewsSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  category: { type:String, required: true},
-  mainImage: { type: String, required: true },
-  body: [{ type: Map, of: String }],
-  date: { type: Date, default: Date.now }
+    title: { type: String, required: true },
+    category: { type:String, required: true},
+    mainImage: { type: String, required: true },
+    body: [{ type: Map, of: String }],
+    date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('News', NewsSchema);
