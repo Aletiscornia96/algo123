a6ae6dd19e923c288ae192eeeecb8f915bc60077
Ale <aletiscornia96@gmail.com>
2024-09-04 11:47:04 -0300

    config basica

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const path = require('path');
+const newsRoutes = require('./routes/news');
 
 const app = express();
 const port = 5000;
 
 // Conectar a MongoDB
 mongoose.connect('mongodb://<usuario>:<contraseña>@<host>:<puerto>/<base_de_datos>', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+    .catch(err => console.error(err));
 
 // Middleware
 app.use(express.json());
@@ -19,20 +20,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Configuración de Multer para la carga de archivos
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
+    destination: (req, file, cb) => {
     cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
+    },
+    filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
-  }
+    }
 });
 const upload = multer({ storage });
 
 // Rutas
 app.post('/upload', upload.single('file'), (req, res) => {
-  res.json({ file: req.file });
+    res.json({ file: req.file });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
+
+
+app.use('/news', newsRoutes);
