3f33e14f3eaf3ea7f3cc4fa068f32a8b7e787c9d
Ale <aletiscornia96@gmail.com>
2024-09-06 11:00:52 -0300

    CRUD actualizado

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const News = require('../models/News');
 
 // Crear una nueva noticia
-router.post('/', async (req, res) => {
+router.post('/create', async (req, res) => {
     const { title, mainImage, body } = req.body;
     try {
         const news = new News({ title, mainImage, body });
@@ -21,6 +21,29 @@ router.get('/', async (req, res) => {
         res.json(news);
     } catch (err) {
         es.status(400).json({ error: err.message });
+    }
+});
+
+// Modificar noticia
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, content, category } = req.body;
+    try {
+        const updatedNews = await News.findByIdAndUpdate(id, { title, content, category }, { new: true });
+        res.status(200).json(updatedNews);
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating news' });
+    }
+});
+
+  // Eliminar noticia
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        await News.findByIdAndDelete(id);
+        res.status(200).json({ message: 'News deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting news' });
     }
 });
 
