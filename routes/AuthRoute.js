35324ead70175ec328d6f75cf70136bf5e8f1991
Ale <aletiscornia96@gmail.com>
2024-09-09 10:39:39 -0300

    login agregado

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,6 +1,7 @@
-const { Signup } = require("../controllers/AuthController");
+const { Signup, Login } = require("../controllers/AuthController");
 const router = require("express").Router();
 
 router.post("/signup", Signup);
+routes.post("/login", Login);
 
 module.exports = router;
